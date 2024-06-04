3e6f4e4bf07e7da941dc209d6e833c11b709b61d
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-06-04 15:50:39 +0100

    Add form functionality and styling

---
+++ script.js
@@ -0,0 +1,57 @@
+// Selet items
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+// edit option
+
+let editElement;
+let editFlag = false;
+let editID = '';
+
+// add event listener
+
+form.addEventListener('submit', addItem);
+
+// functions
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+    if(value && !editFlag) {
+        const element = document.createElement('article');
+        let attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.classList.add('grocery-item');
+        element.innerHTML = `<p class="title">${value}</p>
+        <div class="btn-container">
+            <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+            </button>
+            <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+            </button>
+        </div>`;
+        const deleteBtn = element.querySelector('.delete-btn');
+        const editBtn = element.querySelector('.edit-btn');
+        deleteBtn.addEventListener('click', deleteItem);
+        editBtn.addEventListener('click', editItem);
+        list.appendChild(element);
+        displayAlert('item added to the list', 'success');
+        container.classList.add('show-container');
+        addToLocalStorage(id, value);
+        setBackToDefault();
+    } else if(value && editFlag) {
+        editElement.innerHTML = value;
+        displayAlert('value changed', 'success');
+        editLocalStorage(editID, value);
+        setBackToDefault();
+    } else {
+        displayAlert('please enter value', 'danger');
+    }
+}
